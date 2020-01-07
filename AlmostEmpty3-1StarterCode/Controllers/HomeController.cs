aabb3afd4526704f5bba3e0857f8e2ee0e105ddd
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-07 11:26:47 -0800

    Created basic MVC website with a couple pages and a layout

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //The title for this one is set in the file itself
+            return View(); //View("could return a path");
+        }
+    }
+}
